95608c7d0d2e46561d00f3a31df780c60352b4d1
JARVIS\deeps <deepshika2407@gmail.com>
2026-05-11 22:48:27 +0800

    Created City Page based on search results

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,5 +1,68 @@
+import CurrentWeather from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temperature";
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import WeatherDetails from "@/components/weather-details";
+import WeatherForecast from "@/components/weather-forecast";
+import { useForecastQuery, useWeatherQuery } from "@/hooks/use-weather";
+import { AlertTriangle } from "lucide-react";
+import { useParams, useSearchParams } from "react-router-dom";
+
 const CityPage = () => {
-  return <div>CityPage</div>;
+  const [searchParams] = useSearchParams();
+  const params = useParams();
+  const lat = parseFloat(searchParams.get("lat") || "0");
+  const lon = parseFloat(searchParams.get("lon") || "0");
+
+  const coordinates = { lat, lon };
+
+  const weatherQuery = useWeatherQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
+
+  if (weatherQuery.error || forecastQuery.error) {
+    return (
+      <Alert variant="destructive" className="max-w-md p-3">
+        <AlertTriangle className="h-6 w-6 text-destructive mt-1" />
+        <AlertTitle className="text-lg font-bold">Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Failed to fetch weather data. Please try again.</p>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!weatherQuery.data || !forecastQuery.data || !params.cityName) {
+    return <WeatherSkeleton />;
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* Favorite Cities List */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold tracking-tight">
+          {params.cityName}, {weatherQuery.data.sys.country}
+        </h1>
+        <div>{/* Favorite Button */}</div>
+      </div>
+
+      {/* Current and Hourly Weather Information */}
+      <div className="grid gap-6">
+        <div className="flex flex-col gap-4">
+          {/* Current Weather */}
+          <CurrentWeather data={weatherQuery.data} />
+          {/* Hourly Temperature */}
+          <HourlyTemperature data={forecastQuery.data} />
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-2 items-start">
+          {/* Weather Details */}
+          <WeatherDetails data={weatherQuery.data} />
+          {/* 5 days Forecast */}
+          <WeatherForecast data={forecastQuery.data} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CityPage;
